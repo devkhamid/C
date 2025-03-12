aca105b330a310e0daad6d06968652dc70e6547c
devkhamid <hsiddiqxojayev@gmail.com>
2025-03-12 12:20:05 +0500

    first commit

---
+++ main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int main(void)
+{
+    int b;
+    printf("Enter a value: ");
+    scanf("%d", &b);
+    if (b < 0)
+    {
+        printf("The value is negative\n");
+    }
+    else if (b == 0)
+    {
+        printf("The value is zero\n");
+    }
+    else
+    {
+        printf("The value is positive\n");
+    }
+    return 0;
+}
